ae3d201fcf53abd881c32a8bd9bd85559249c3dc
Jarryd <jarryd.webb@hotmail.com>
2023-01-19 09:11:35 +1100

    Update 121

--- leet_py/BestTimeToBuyAndSellStock_121.py
+++ leet_py/BestTimeToBuyAndSellStock_121.py
@@ -5,23 +5,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        lmax, gmax = 0, -inf
-        for i, v in enumerate(prices):
-            if i + 1 == len(prices): break
-            lmax = max(prices[i + 1] - prices[i], prices[i + 1] - prices[i] + lmax)
-            if lmax > gmax: gmax = lmax
-
-        return gmax if gmax > 0 else 0
-
-    def maxProfit(self, prices: List[int]) -> int:
-
-        profit, lowest = 0, inf
+        lowest, profit = inf, 0
         for price in prices:
-            if lowest > price:
-                lowest = price
-            
-            if price - lowest > profit:
-                profit = price - lowest
+            lowest = min(price, lowest)
+            profit = max(profit, price - lowest)
 
         return profit
 
