0323b24fc8d9895557d0f41c2f7c24305788635c
Jarryd <jarryd.webb@hotmail.com>
2023-01-23 15:07:49 +1100

    Update 3

--- leet_py/LongestSubstringWithoutRepeatingCharacters_3.py
+++ leet_py/LongestSubstringWithoutRepeatingCharacters_3.py
@@ -4,17 +4,26 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 1: return 1
 
-        out, longest = [], 0
-        for ch in s:
-            if ch not in out:
-                out.append(ch)
-            else:
-                out = out[out.index(ch) + 1:]
-                out.append(ch)
+        # O(n^2) Naive
+            # arr, lar = [], 0
+            # for ch in s:
+            #     if ch in arr:
+            #         arr = arr[arr.index(ch) + 1:]
 
-            print(out)
-            if len(out) > longest:
-                longest = len(out)
+            #     arr.append(ch)
+            #     lar = max(lar, len(arr))
+            
+            # return lar
 
-        return longest
+        # O(n) Two Pointer
+        output, left, seen = 0, 0, {}
+        for right, val in enumerate(s):
+            if val in seen:
+                left = max(left, seen[val] + 1)
+            output = max(output, (right - left) + 1)
+            seen[val] = right
+        
+
+        return output 
