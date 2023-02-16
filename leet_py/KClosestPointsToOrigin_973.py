71d2fd94f5f5ca889ab93d3f199519cc543b33f1
Jarryd <jarryd.webb@hotmail.com>
2023-02-16 13:43:22 +1100

    Add 973

--- leet_py/KClosestPointsToOrigin_973.py
+++ leet_py/KClosestPointsToOrigin_973.py
@@ -1,9 +1,23 @@
 
-# runtime: O(n)
-# space:   O(n)
+# runtime: O(n * logk)
+# space:   O(k)
+
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+# 
+#         return sorted(points, key = lambda x:x[0]**2 + x[1]**2)[:k]
+
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
-        return sorted(points, key = lambda x:x[0]**2 + x[1]**2)[:k]
+        heap = []
+        for (x, y) in points:
+            dist = -1 * (x**2 + y**2)
+            if len(heap) == k:
+                # pushes item on the heap, then pops and returns the smallest item from the heap
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
 
+        return [(x, y) for (dist, x, y) in heap]
